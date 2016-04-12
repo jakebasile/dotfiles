4e58add8d4c61ae85b2d35f8ecbb5e58f8007cfd
Jake Basile <jake@jakebasile.com>
2016-04-12 15:29:27 -0400

    make vim backup dir

--- scripts/vim.zsh
+++ scripts/vim.zsh
@@ -2,6 +2,7 @@
 # Install my Vim plugins
 
 mkdir -p ~/.vim/{autoload,bundle}
+mkdir ~/.vimbackups
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 for vim_plugin in $( cat conf/vim-plugins.txt ); do
