d1881db02a1a33778c680a5296c69f1d3b38e36b
Jake Basile <git@jakebasile.com>
2019-03-04 17:06:56 -0600

    updates
    updates

--- files/lein/profiles.clj
+++ files/lein/profiles.clj
@@ -1,6 +1,3 @@
-{:ppedn {:dependencies [[org.clojure/clojure "1.6.0"]
+{:ppedn {:dependencies [[org.clojure/clojure "1.9.0"]
                         [mvxcvi/puget "0.8.0"]]}
- :user {:dependencies [[proto-repl "0.3.1"]
-                       [cljfmt "0.5.1"]]
-        :repl-options {:init (require 'cljfmt.core)}
-        :plugins [[cider/cider-nrepl "0.17.0"]]}} 
+ :user {:dependencies [[proto-repl "0.3.1"]]}}
