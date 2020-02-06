cfb4ea7c078524d4718ecbfc51e73e057a210154
Jake Basile <git@jakebasile.com>
2020-02-06 10:53:42 -0600

    you're not the boss of me now

--- files/lein/profiles.clj
+++ files/lein/profiles.clj
@@ -1,3 +1,4 @@
 {:ppedn {:dependencies [[org.clojure/clojure "1.9.0"]
                         [mvxcvi/puget "0.8.0"]]}
- :user {:dependencies [[proto-repl "0.3.1"]]}}
+ :user {:dependencies [[proto-repl "0.3.1"]]
+        :jvm-opts ["-Dclojure.server.myrepl={:port,9001,:accept,clojure.core.server/repl}"]}}
