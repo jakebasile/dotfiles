e2bdf910c64d3487a0e80286fb151218674ea1c9
Jake Basile <git@jakebasile.com>
2020-02-20 13:21:55 -0600

    asdasd

--- files/lein/profiles.clj
+++ files/lein/profiles.clj
@@ -1,4 +1,3 @@
 {:ppedn {:dependencies [[org.clojure/clojure "1.9.0"]
                         [mvxcvi/puget "0.8.0"]]}
- :user {:dependencies [[proto-repl "0.3.1"]]
-        :jvm-opts ["-Dclojure.server.myrepl={:port,9001,:accept,clojure.core.server/repl}"]}}
+ :user {:jvm-opts ["-Dclojure.server.myrepl={:port,9001,:accept,clojure.core.server/repl}"]}}
