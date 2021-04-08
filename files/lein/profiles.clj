88640a86dd8e5baf8e7fb2c153974c54ff4cd478
Jake Basile <git@jakebasile.com>
2021-04-08 11:20:41 -0500

    force normal map printing in lein repls

--- files/lein/profiles.clj
+++ files/lein/profiles.clj
@@ -3,4 +3,8 @@
  :user {:dependencies [[hashp "0.1.1"]
                        [criterium "0.4.6"]]
         :injections [(require 'hashp.core)
-                     (require '[criterium.core :as cr])]}}
+                     (require '[criterium.core :as cr])
+                     (defmethod print-method clojure.lang.IPersistentMap [m, ^java.io.Writer w]
+                       (#'clojure.core/print-meta m w)
+                       (#'clojure.core/print-map m #'clojure.core/pr-on w))]}}
+
