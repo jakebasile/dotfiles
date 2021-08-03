18bc3475d422eef8825a8db87ebe3a3fd322c2ae
Jake Basile <git@jakebasile.com>
2021-08-03 09:45:46 -0500

    update profiles.lein

--- files/lein/profiles.clj
+++ files/lein/profiles.clj
@@ -1,13 +1,5 @@
-{:ppedn {:dependencies [[org.clojure/clojure "1.9.0"]
-                        [mvxcvi/puget "0.8.0"]]}
- :user {:dependencies [[hashp "0.1.1"]
-                       [criterium "0.4.6"]]
+{:user {:dependencies [[pjstadig/humane-test-output "0.11.0"]
+                       [hashp "0.2.1"]]
         :injections [(require 'hashp.core)
-                     (require '[criterium.core :as cr])
-                     (defmethod print-method clojure.lang.IPersistentMap [m, ^java.io.Writer w]
-                       (#'clojure.core/print-meta m w)
-                       (#'clojure.core/print-map m #'clojure.core/pr-on w))
-                     (defmethod clojure.pprint/simple-dispatch clojure.lang.IPersistentMap
-                       [m]
-                       (with-redefs [clojure.core/lift-ns (constantly nil)]
-                         (#'clojure.pprint/pprint-map m)))]}}
+                     (require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
